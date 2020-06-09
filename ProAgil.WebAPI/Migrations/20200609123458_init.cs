b890989ce3c6253ebf8083fa2a5841bc653c10f5
Silvio Almeida <silvioass@gmail.com>
2020-06-09 11:29:25 -0300

    Firt commit

---
+++ ProAgil.WebAPI/Migrations/20200609123458_init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Tema = table.Column<string>(nullable: true),
+                    Local = table.Column<string>(nullable: true),
+                    Lote = table.Column<string>(nullable: true),
+                    QtdPessoas = table.Column<int>(nullable: false),
+                    DataEvento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.EventoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
